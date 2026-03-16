e7042a64e24d437a60e99cb5ecafafe8405e44d1
Hellfyre23 <xboxemail303@gmail.com>
2026-03-16 19:10:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,5 @@ import discord
 from discord.ext import commands
 import io
 
-TOKEN = "2198ca7c61283ff5ded47cc9550736af3404a1e0da94fd91a3bb5db5b4915d37"
+import os
+TOKEN = os.getenv("TOKEN")  # <-- THIS is how Railway reads secrets
